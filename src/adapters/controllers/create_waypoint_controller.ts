edb61ed2be11c7215007b1ba607338e8231ce65f
CePseudoBE <axel.hoffmann0205@yahoo.com>
2024-09-27 19:25:16 +0200

    feat : inject use_case in controller, needs to handle validation

--- src/adapters/controllers/create_waypoint_controller.ts
+++ src/adapters/controllers/create_waypoint_controller.ts
@@ -1,8 +1,15 @@
 import { HttpContext } from '@adonisjs/core/http'
+import { CreateWaypointUseCase } from '#domain/use_cases/waypoints/create_waypoint_use_case'
+import { inject } from '@adonisjs/core'
 
+@inject()
 export default class CreateWaypointController {
+  constructor(private createWaypointUseCase: CreateWaypointUseCase) {}
+
   handle({ request }: HttpContext) {
     const body = request.body()
     console.log(body)
+    this.createWaypointUseCase.handle(body)
+    return null
   }
 }
