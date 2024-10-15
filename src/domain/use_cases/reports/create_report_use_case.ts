a2bb72bb4ef5a54d0783d78e0004e2baca849644
CePseudoBE <axel.hoffmann0205@yahoo.com>
2024-10-15 16:33:09 +0200

    feat(report): added youtrack to usecase

--- src/domain/use_cases/reports/create_report_use_case.ts
+++ src/domain/use_cases/reports/create_report_use_case.ts
@@ -1,10 +1,14 @@
 import { inject } from '@adonisjs/core'
 import { IReportRepository } from '#domain/repositories/ireport_repository'
 import { Report } from '#domain/entities/report'
+import { IYoutrackService } from '#domain/services/iyou_track_service'
 
 @inject()
 export class CreateReportUseCase {
-  constructor(private iReportRepository: IReportRepository) {}
+  constructor(
+    private iReportRepository: IReportRepository,
+    private iYourTrackService: IYoutrackService
+  ) {}
 
   public async handle(data: {
     sessionId: string
@@ -23,6 +27,10 @@ export class CreateReportUseCase {
       data.contact
     )
 
-    return await this.iReportRepository.create(report)
+    const reportCreated = await this.iReportRepository.create(report)
+
+    await this.iYourTrackService.createIssue(report)
+
+    return reportCreated
   }
 }
