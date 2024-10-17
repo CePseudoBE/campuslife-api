d74dbbe08a463b64eab00ce9aeafad9ad6210a25
CePseudoBE <axel.hoffmann0205@yahoo.com>
2024-10-17 14:59:13 +0200

    feat(country): make repo

---
+++ src/domain/repositories/icountry_repository.ts
@@ -0,0 +1,12 @@
+import { QueryParams } from '#domain/services/sorting_validation'
+import { Country } from '#domain/entities/country'
+
+export abstract class ICountryRepository {
+  abstract create(country: Country): Promise<Country>
+
+  abstract findById(id: number): Promise<Country>
+
+  abstract findAll({ page, limit, order, column }: QueryParams): Promise<Country[]>
+
+  abstract delete(country: Country): Promise<null>
+}
---
+++ src/infrastructure/orm/repositories/country_repository.ts
@@ -0,0 +1,53 @@
+import { ICountryRepository } from '#domain/repositories/icountry_repository'
+import { inject } from '@adonisjs/core'
+import { Country } from '#domain/entities/country'
+import { QueryParams } from '#domain/services/sorting_validation'
+import { CountryMapper } from '#adapters/mappers/country_mapper'
+import CountryModel from '#infrastructure/orm/models/country_model'
+
+@inject()
+export class CountryRepository extends ICountryRepository {
+  constructor() {
+    super()
+  }
+
+  async create(country: Country): Promise<Country> {
+    const countryModel = CountryMapper.toPersistence(country)
+    await countryModel.save()
+    return CountryMapper.toDomain(countryModel)
+  }
+
+  async delete(country: Country): Promise<null> {
+    if (!country.id) {
+      throw new Error('NotFound: Country not found')
+    }
+
+    const countryModel = await CountryModel.findOrFail(country.id)
+
+    await countryModel.delete()
+
+    return null
+  }
+
+  async findAll({ page, limit, order, column }: QueryParams): Promise<Country[]> {
+    const query = CountryModel.query()
+
+    if (page && limit) {
+      await query.paginate(page, limit)
+    }
+
+    if (column && order) {
+      query.orderBy(column, order)
+    }
+
+    const countryModels = await query.exec()
+
+    return countryModels.map((country) => CountryMapper.toDomain(country))
+  }
+
+  async findById(id: number): Promise<Country> {
+    const countryModel = await CountryModel.findOrFail(id)
+
+    return CountryMapper.toDomain(countryModel)
+  }
+}
